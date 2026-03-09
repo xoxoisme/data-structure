fa2be2d810e6e3f58df8fde9f899b0484f2b1063
xoxoisme <kxoxo3333@gmail.com>
2026-03-09 20:54:36 +0900

    add: 배열 구현해보기

--- array.py
+++ array.py
@@ -3,7 +3,7 @@ class array:
         self.size = size
         self.data = [None] * size
 
-    def __getitem__(self, index):
+    def __getitem(self, index):  # 인덱스가 0보다 작거나 size보다 크면 에러 발생
         if index < 0 or index >= self.size:
             raise IndexError("Index out of bounds")
         return self.data[index]
@@ -18,16 +18,17 @@ class array:
 
     def __str__(self):
         return str(self.data)
-# Example usage:
+    
+# 예제 사용법:
 if __name__ == "__main__":
     arr = array(5)
     arr[0] = 10
     arr[1] = 20
     arr[2] = 30
-    print(arr)  # Output: [10, 20, 30, None, None]
-    print(arr[1])  # Output: 20
-    print(len(arr))  # Output: 5
+    print(arr)  # 출력: [10, 20, 30, None, None]
+    print(arr[1])  # 출력: 20
+    print(len(arr))  # 출력: 5
     try:
-        print(arr[5])  # This will raise an IndexError
+        print(arr[5])  # 에러 발생
     except IndexError as e:
-        print(e)  # Output: Index out of bounds
+        print(e)  # 출력: 인덱스 범위 초과
